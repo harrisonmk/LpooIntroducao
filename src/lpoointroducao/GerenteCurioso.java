11eb272ca7b1de90605303d37930fc561ee6b580
Harrison <Harrison@192.168.1.3>
2018-03-08 20:15:02 -0300

    Gerente

--- src/lpoointroducao/GerenteCurioso.java
+++ src/lpoointroducao/GerenteCurioso.java
@@ -6,7 +6,7 @@ public class GerenteCurioso {
     
     public Conta clienteMaiorValor (Conta p1,Conta p2){
         
-      if (p1.getSaldo() >= p2.getSaldo()) {
+      if (p1.getSaldo() > p2.getSaldo()) {
             return p1;
         } else {
             
